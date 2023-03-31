4a33e5c8e7b541651703ba3c83e003fe7786ffab
jonybearr <65643049+jonybearr@users.noreply.github.com>
2023-03-31 19:46:51 +0800

    finished simple GUI version
    redesigned logic flow of program to utilize the run on click property of event listeners and made basic gui buttons

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 console.log('hello there')
 
+//helper functions
 
 //randomly generates a rock, paper or scissors for the computer's move
 function getComputerChoice() {
@@ -16,6 +17,7 @@ function getComputerChoice() {
     }
 }
 
+//determines winner based on string input of player and computer moves
 function playRound(playerSelection, computerSelection) {
     //convert to lowercase
     playerSelection = playerSelection.toLowerCase();
@@ -24,68 +26,66 @@ function playRound(playerSelection, computerSelection) {
     console.log('computer chose '+computerSelection)
     //determine winner for each possibility or tie
     if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('computer won')
-        return 'computer'
+        console.log('computer won this round')
+        computerScore++;
     }else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('player won')
-        return 'player'
+        console.log('player won this round')
+        playerScore++;
     }else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        console.log('computer won')
-        return 'computer'
+        console.log('computer won this round')
+        computerScore++;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('player won')
-        return 'player'
+        console.log('player won this round')
+        playerScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        console.log('computer won')
-        return 'computer'
+        console.log('computer won this round')
+        computerScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('player won')
-        return 'player'
+        console.log('player won this round')
+        playerScore++;
     } else {
-        return 'tie'
+        //do nothing if it is a tie
     }
 
-}
+    console.log('the current score is ');
+    console.log('player ', playerScore);
+    console.log('computer ',computerScore);
 
-//simplifies user input of three choices to numbers 1 2 and 3
-function userInput() {
-    let inputValue=prompt('enter 1 for rock, 2 for paper, and 3 for scissors');
-    if (inputValue == 1) {
-        return 'rock'
-    } else if (inputValue == 2) {
-        return 'paper'
-    } else if (inputValue == 3) {
-        return 'scissors'
-}
 }
 
 
-//game loop
-function game() {
-    console.log('this is a 5 round majority wins game of rock paper scissors!');
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i=0; i<5; i++){
-        let result = playRound(userInput(),getComputerChoice());
-        if (result == 'player') {
-            playerScore++;
-        } else if (result == 'computer') {
-            computerScore++;
-        } else {
-            //do nothing if theres a tie
-        }
-    }
-
-    //after 5 rounds annouce final winners
-    console.log('rounds player won ' + playerScore);
-    console.log('rounds computer won ' +computerScore);
+function finish() {
+    console.log('the game has finished! here are the results')
+    console.log('player score: ',playerScore);
+    console.log('computer score: ',computerScore);
     if(playerScore>computerScore){
-        console.log('player won!')
-    } else if (playerScore<computerScore){
-        console.log('computer won!')
-    } else {
-        console.log('it was a tie!')
+        console.log("player won!")
+    } else if (computerScore>playerScore){
+        console.log("oof computer won")
+    }else{
+        console.log("it was a tie!")
+    
     }
+    //reset scores
+    playerScore=0;
+    computerScore=0;
 }
 
-game();
+//user input upgraded to use GUI instead of prompt
+
+//establish counter variables
+let playerScore = 0;
+let computerScore = 0;
+
+
+//establish js variables from DOM
+const rockButton=document.querySelector("#rock");
+const paperButton=document.querySelector("#paper");
+const scissorsButton=document.querySelector("#scissors");
+const finishButton=document.querySelector("#finish");
+
+//add function reactions to them
+rockButton.addEventListener("click", function(){playRound('rock',getComputerChoice())});
+paperButton.addEventListener("click", function(){playRound('paper',getComputerChoice())});
+scissorsButton.addEventListener("click", function(){playRound('scissors',getComputerChoice())});
+finishButton.addEventListener('click', function(){finish()});
